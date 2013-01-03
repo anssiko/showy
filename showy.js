b5b51d5171c990ddd671641fb8e3b5c08a461eba
Anssi Kostiainen <anssi.kostiainen@gmail.com>
2013-01-03 17:52:35 +0200

    implement preview image rotation

--- showy.js
+++ showy.js
@@ -14,7 +14,8 @@ var body = q('body'),
     prev_button = q('#prev'),
     next_button = q('#next'),
     back_button = q('#back'),
-    fullscreen_button = q('#fullscreen');
+    fullscreen_button = q('#fullscreen'),
+    rotate_button = q('#rotate');
 
 function addImages(files) {
   var i, img;
@@ -49,6 +50,7 @@ function showThumbnails() {
   preview.hidden = true;
   metadata.hidden = true;
   range.hidden = true;
+  rotate_button.hidden = true;
   thumbnails.hidden = false;
   prev_button.hidden = true;
   next_button.hidden = true;
@@ -75,6 +77,7 @@ function showPreview(img) {
   input.hidden = true;
   metadata.hidden = false;
   range.hidden = false;
+  rotate_button.hidden = false;
   thumbnails.hidden = true;
   info.hidden = false;
   prev_button.hidden = false;
@@ -396,20 +399,31 @@ function getCurrentImageSize(img, zoom_ratio, screen_width, screen_height) {
   return { width: img_current_width, height: img_current_height };
 }
 
-function scaleImage(img, scale_ratio) {
+function transformImage(img, scale_ratio, rotate_deg) {
   var screen_width = window.innerWidth,
-      screen_height = window.innerHeight;
+      screen_height = window.innerHeight,
+      rotate_deg = parseInt(rotate_deg, 10);
       
   var img_current = getCurrentImageSize(img, scale_ratio, screen_width, screen_height);
-  info.textContent = Math.round(scale_ratio * 100) + ' % (' + Math.round(img_current.width, 10) + 'x' + Math.round(img_current.height, 10) + ')';
+  info.textContent = Math.round(scale_ratio * 100) + ' % (' +
+                     Math.round(img_current.width, 10) + 'x' +
+                     Math.round(img_current.height, 10) + ')';
 
   var transform = Modernizr.prefixed('transform');
   var transformOriginX = Modernizr.prefixed('transformOriginX');
   var transformOriginY = Modernizr.prefixed('transformOriginY');
+  var rotated = (rotate_deg !== 0);
 
-  img.style[transformOriginX] = (img_current.width > screen_width) ? '0' : '50%';
-  img.style[transformOriginY] = (img_current.height > screen_height) ? '0' : '50%';
-  img.style[transform] = 'scale(' + scale_ratio + ')';
+  if (rotated) {
+    var img_prev_width = img_current.width;
+    var img_prev_height = img_current.height;
+    img_current.width = img_prev_height;
+    img_current.height = img_prev_width;
+  }
+
+  img.style[transformOriginX] = (!rotated && img_current.width > screen_width) ? '0' : '50%';
+  img.style[transformOriginY] = (!rotated && img_current.height > screen_height) ? '0' : '50%';
+  img.style[transform] = 'scale(' + scale_ratio + ') rotate(' + rotate_deg + 'deg)';
 
   body.style.overflowX = (img_current.width > screen_width) ? 'scroll' : 'hidden';
   body.style.overflowY = (img_current.height > screen_height - 50) ? 'scroll' : 'hidden';
@@ -421,7 +435,7 @@ function init() {
   }
   
   range.onchange = function () {
-    scaleImage(q('#preview > img'), range.value / 100);
+    transformImage(q('#preview > img'), range.value / 100, rotate.dataset.deg);
   };
 
   input.onchange = function () {
@@ -438,6 +452,21 @@ function init() {
 
   next_button.onclick = function () {
     showNextImage();
+  };
+  
+  rotate_button.onclick = function () {
+    var img = q('#preview > img'),
+        prev_width = img.dataset.width,
+        prev_height = img.dataset.height;
+    
+    img.dataset.width = prev_height;
+    img.dataset.height = prev_width;
+    
+    this.dataset.deg = (parseInt(this.dataset.deg, 10) === 270) ?
+                       0 : parseInt(this.dataset.deg, 10) + 90;
+
+    transformImage(q('#preview > img'), range.value / 100, rotate.dataset.deg);
+    this.style.webkitTransform = 'rotate(' + rotate.dataset.deg + 'deg)';
   };
   
   // FIXME - dropzone disabled
